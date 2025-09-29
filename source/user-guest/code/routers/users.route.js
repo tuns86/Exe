5fbeb3e48bdc990cde17676a973a099cf19b584b
Tuns <nguyennttuan8604@gmail.com>
2025-09-29 10:38:53 +0700

    fix sth

--- source/user-guest/code/routers/users.route.js
+++ source/user-guest/code/routers/users.route.js
@@ -15,7 +15,7 @@ const nodemailer = require("nodemailer");
 
 // const { google } = require("googleapis");
 
-const OAuth2 = google.auth.OAuth2;
+// const OAuth2 = google.auth.OAuth2;
 
 const bcrypt = require("bcryptjs");
 
