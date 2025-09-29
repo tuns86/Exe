0bbbdeb8e6c0ea5c12a522895f17918d9a223e8b
tuns86 <164044322+tuns86@users.noreply.github.com>
2025-09-29 09:37:31 +0700

    Add files via upload

---
+++ source/user-guest/code/config/cloudinary.config.js
@@ -0,0 +1,7 @@
+module.exports = function(cloudinary) {
+    cloudinary.config({
+        cloud_name: 'teamwebctt2',
+        api_key: '983121122945713',
+        api_secret: 'gulRf36137BySnsuZHIFkRZqT-U'
+    })
+};
