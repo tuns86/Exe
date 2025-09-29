bfe38450403b3ed8c89838dece3112f0452b9e83
tuns86 <164044322+tuns86@users.noreply.github.com>
2025-09-29 09:54:35 +0700

    Update key.config.js

--- source/user-guest/code/config/key.config.js
+++ source/user-guest/code/config/key.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoLocal: 'mongodb+srv://tuns86_db_user:<db_password>@cluster0.ivynwqb.mongodb.net/'
+    MongoLocal: 'mongodb+srv://tuns86_db_user:connectmentorexe@cluster0.ivynwqb.mongodb.net/EXE?retryWrites=true&w=majority'
 }
