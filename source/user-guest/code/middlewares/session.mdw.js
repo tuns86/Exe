0d49a37fa55883675f7220d034506fddfc6070c0
Tuns <nguyennttuan8604@gmail.com>
2025-10-14 10:25:25 +0700

    Update session

--- source/user-guest/code/middlewares/session.mdw.js
+++ source/user-guest/code/middlewares/session.mdw.js
@@ -1,11 +1,19 @@
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const { MongoLocal } = require("../config/key.config");
 
 module.exports = function (app) {
-    // Express session
     app.use(session({
         secret: "secret",
         resave: false,
         saveUninitialized: false,
-        cookie: { maxAge: 1000 * 60 * 60 },
+        store: MongoStore.create({
+            mongoUrl: MongoLocal,
+            collectionName: "sessions"
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60, // 1 hour
+            httpOnly: true
+        }
     }));
 };
