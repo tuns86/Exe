164e3ff78a340f98f98f44063ca3dbd198c2adbe
Tuns <nguyennttuan8604@gmail.com>
2025-11-04 08:49:50 +0700

    change session to 24 hours

--- source/user-guest/code/middlewares/session.mdw.js
+++ source/user-guest/code/middlewares/session.mdw.js
@@ -7,12 +7,8 @@ module.exports = function (app) {
         secret: "secret",
         resave: false,
         saveUninitialized: false,
-        store: MongoStore.create({
-            mongoUrl: MongoLocal,
-            collectionName: "sessions"
-        }),
         cookie: {
-            maxAge: 1000 * 60 * 60, // 1 hour
+            maxAge: 1000 * 60 * 60 * 24, // 24 hour
             httpOnly: true
         }
     }));
